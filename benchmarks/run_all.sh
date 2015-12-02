3878de2c394880145b4b5d938d8baad0de0be37e
raswaminathan <rs268@duke.edu>
2015-12-02 15:29:10 -0800

    run_all script

---
+++ benchmarks/run_all.sh
@@ -0,0 +1,9 @@
+./run_benchmark.sh RUNgcc gcc00.peak.ev6 true false ../markovData/gcc_32_64.txt
+
+./run_benchmark.sh RUNcrafty crafty00.peak.ev6 true false ../markovData/crafty_32_64.txt
+
+./run_benchmark.sh RUNvortex vortex00.peak.ev6 true false ../markovData/vortex_32_64.txt
+
+./run_benchmark.sh RUNmgrid mgrid00.peak.ev6 true false ../markovData/mgrid_32_64.txt
+
+./run_benchmark.sh RUNtwolf twolf00.peak.ev6 true false ../markovData/twolf_32_64.txt
