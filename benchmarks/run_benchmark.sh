87664761b2868d88f2ea9e31389c486005a4593f
raswaminathan <rs268@duke.edu>
2015-12-02 11:42:55 -0800

    modified run benchmark script to take more args

--- benchmarks/run_benchmark.sh
+++ benchmarks/run_benchmark.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]
+if [ $# -lt 4 ]
   then
-    echo "Need 3 arguments: benchmark, binary, file"
+    echo "Need at least 4 arguments: benchmark, binary, markovEnabled (true or false), strideEnabled (true or false), file to pipe stderr/stdout (optional)"
     exit
 fi
 
 benchmark=$1
 binary=$2
-file=$3
+markovEnabled=$3
+strideEnabled=$4
 
-./$benchmark ../simple-scalar/final_project ./$binary -max:inst 50000000 -fastfwd 20000000 > $file 2>&1
+if [ $# -eq 5 ] 
+  then
+    file=$5
+    ./$benchmark ../simple-scalar/final_project ./$binary -markovprefetching:enabled $markovEnabled -prefetchcache:enabled $strideEnabled -max:inst 50000000 -fastfwd 		20000000 > $file 2>&1
+
+  else 
+
+     ./$benchmark ../simple-scalar/final_project ./$binary -markovprefetching:enabled $markovEnabled -prefetchcache:enabled $strideEnabled -max:inst 50000000 -fastfwd 		20000000
+fi
+
+
+
+
+
