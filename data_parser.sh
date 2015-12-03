15a2f6604fd0cf79317d6962d71e97907545f721
raswaminathan <rs268@duke.edu>
2015-12-02 16:37:36 -0800

    gathered markov data

--- data_parser.sh
+++ data_parser.sh
@@ -1,15 +1,54 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
   then
-    echo "Need 1 argument: file to parse"
+    echo "Need 2 arguments: file to parse and csv file"
     exit
 fi
 
 file=$1
+underscore='_'
+dot='.'
+currentString=''
+currentIndex=0;
+benchmarkName=''
+tableSize=''
+bufferSize=''
+
+for (( i=0; i<${#file}; i++ )); do
+  if [ "${file:$i:1}" = "$dot" ] 
+    then
+      break
+  fi
+  if [ "${file:$i:1}" = "$underscore" ] 
+    then
+    if [ "$currentIndex" -eq 0 ]
+      then
+        benchmarkName=$currentString
+	currentString=''
+	currentIndex=1
+      else
+        tableSize=$currentString
+	currentString=''
+    fi
+    
+    else 
+      currentString=$currentString${file:$i:1}
+  fi
+done
+
+bufferSize=$currentString
+
+csv=$2
 l1_misses=""
 l1_miss_rate=""
 l1_accesses=""
+l2_misses=""
+l2_miss_rate=""
+l2_misses=""
+stride_hits=""
+markov_hits=""
+IPC=""
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	arr=( $line );
 	name=${arr[0]};
@@ -18,6 +57,12 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	dl1Misses='dl1.misses';
 	dl1MissRate='dl1.miss_rate';
 	dl1Accesses='dl1.accesses';
+	dl2Misses='ul2.misses';
+	dl2MissRate='ul2.miss_rate';
+	dl2Accesses='ul2.accesses';
+	markovHits='sim_number_prefetch_buffer_hits';
+	strideHits='sim_number_prefetch_cache_hits';
+	SIM_IPC='sim_IPC';
 
 	if [ "$name" = "$dl1Misses" ];
 	then
@@ -33,8 +78,38 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	then
 		l1_accesses="$next"
 	fi
+
+	if [ "$name" = "$dl2Misses" ];
+	then
+		l2_misses="$next"
+	fi
+
+	if [ "$name" = "$dl2MissRate" ];
+	then
+		l2_miss_rate="$next"
+	fi
+
+	if [ "$name" = "$dl2Accesses" ];
+	then
+		l2_accesses="$next"
+	fi
+
+	if [ "$name" = "$markovHits" ];
+	then
+		markov_hits="$next"
+	fi
+
+	if [ "$name" = "$strideHits" ];
+	then
+		stride_hits="$next"
+	fi
+
+	if [ "$name" = "$SIM_IPC" ];
+	then
+		IPC="$next"
+	fi
 done < "$file"
 
-echo "$file,$l1_accesses,$l1_misses,$l1_miss_rate" >> default_data.csv
+echo "$benchmarkName,$tableSize,$bufferSize,$l1_accesses,$l1_misses,$l1_miss_rate,$l2_accesses,$l2_misses,$l2_miss_rate,$stride_hits,$markov_hits,$IPC" >> $csv
 
 
